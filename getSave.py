accaed6479c7ae127e487ff7d510f5e166425cbb
ebsaway <edbseymour@gmail.com>
2018-02-11 12:53:39 +0000

    updated function defintions in getSave

--- getSave.py
+++ getSave.py
@@ -1,13 +1,13 @@
 import pickle
 
 def get(filename):
-    """retrieves list from file"""
-    theList = []
+    """retrieves object from file"""
+    theObject = []
     with open(filename, "rb") as fp:
-        theList = pickle.load(fp)
-    return theList
+        theObject = pickle.load(fp)
+    return theObject
 
-def save(theList, filename):
-    """saves list to file"""
+def save(theObject, filename):
+    """saves object to file"""
     with open(filename, "wb") as fp:
-        pickle.dump(theList, fp)
+        pickle.dump(theObject, fp)
