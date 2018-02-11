47af669d74628c848fccec9e43a607f068d6936e
ebsaway <edbseymour@gmail.com>
2018-02-11 17:47:18 +0000

    Thanks MageJohn

--- bot.py
+++ bot.py
@@ -3,16 +3,17 @@ from discord.ext import commands
 import getSave
 import ticTacToe
 from PIL import Image
-import os
 
 # TO DO:
 # add turn as an attribute of gamerooms.txt - Done!
 
 TOKEN="token.txt"
 
+NUM_GAMEROOMS = 4
+
 description = '''CRC Gamemaster Bot'''
 bot = commands.Bot(command_prefix='$', description=description)
-knownGames = ("tic-tac-toe")
+GAMES = {"tic-tac-toe": (2, 2, ticTacToe.TicTacToe)} # format: {gameName: (minPlayers, maxPlayers, gameObject) ...}
 serverName = 'CRC Buddies'
 
 @bot.event
@@ -53,67 +54,81 @@ async def rooms():
     if anyGames:
         await bot.say(response)
     else:
-        await bot.say("There are no games currently in progress")
+        await bot.say("There are no games currently in progress.")
 
 @bot.command()
-async def helpttt():
-    """Prints out board numbers"""
-    await bot.say("```0|1|2\n-----\n3|4|5\n-----\n6|7|8```")
+async def guide(game : str):
+    """Outputs guide to game"""
+    if game in knownGames:
+        if game == "tic-tac-toe":
+            await bot.say("The board positions are:\n```0|1|2\n-----\n3|4|5\n-----\n6|7|8```")
+        #other games go here
+    else:
+        await bot.say("I'm sorry, I don't know that game")
 
-@bot.command()
-async def emptyAll():
-    """empties all game rooms. Plz don't abuse"""
-    gamerooms = [[],[],[],[]]
-    for i in range(4):
-        lServers = list(bot.servers)
-        server = lServers[0]
-        channelName = "gameroom-" + str(i+1)
-        channel = discord.utils.get(bot.get_all_channels(), server__name='CRC Buddies', name=channelName)
+@bot.command(pass_context=True)
+async def emptyRoom(ctx, roomNo : int):
+    """empties a specified gameroom. Plz don't abuse"""
+    if roomNo < 1 or roomNo > NUM_GAMEROOMS:
+        await bot.say("There is no gameroom with number: " + str(roomNo))
+    else:
+        channelName = "gameroom-" + str(roomNo)
+        channel = discord.utils.get(ctx.message.server.channels, name=channelName)
+        gameRooms = getSave.get("gamerooms.txt")
+        gameRooms[int(channelName[9]) - 1] = []
+        getSave.save(gameRooms, "gamerooms.txt")
         await bot.purge_from(channel)
-    getSave.save(gamerooms, "gamerooms.txt")
 
 @bot.command(pass_context=True)
 async def newGame(ctx, *args):
     """starts a new game chosen by the challenger played with the challenged"""
     #args of form e.g. ("martian chess", "player 2", "player 3", "player 4"...)
     #need to check for correct number of players for each game
-    chosenGame = args[0]
-    if len(args) >= 2:
-        if chosenGame in knownGames:
-            gameRooms = getSave.get("gamerooms.txt")
-            mem = ctx.message.server.members
-            names = []
-            for member in mem:
-                names.append(member.name)
+
+    if len(args) < 2:
+        await bot.say("I'm sorry, I didn't understand that. The syntax is (where you are PLAYER1): $newGame GAMENAME PLAYER2 ...")
+    else:
+        chosenGame = args[0]
+        challenged = args[1:]
+        if chosenGame not in GAMES.keys():
+            await bot.say("I'm sorry, I don't know: " + chosenGame)
+        elif len(challenged) + 1 < GAMES[chosenGame][0] or len(challenged) > GAMES[chosenGame][1]:
+            await bot.say("""Wrong number of players!
+            The minimum number of players for this game are {}
+            The maximum number of players for this game are {}
+            """.format(GAMES[chosenGame][0], GAMES[chosenGame][1]))
+        else:
             validPlayers = True
-            challenged = []
-            for i in range(len(args)-1):
-                if args[i+1] not in names:
+            mem = list(ctx.message.server.members)
+            usernames = []
+            for member in mem:
+                usernames.append(member.name)
+            for i in challenged:
+                if i not in usernames:
+                    await bot.say("Those are not valid players!")
                     validPlayers = False
                     break
-                else:
-                    challenged.append(args[i+1])
-
             if validPlayers:
+                gameRooms = getSave.get("gamerooms.txt")
                 success = False 
                 for i in range(len(gameRooms)):
                     if len(gameRooms[i]) < 1:
-                        channelName = "gameroom-" + str(i+1)
-                        channel = discord.utils.get(bot.get_all_channels(), server__name=serverName, name=channelName)
+                        channelName = "gameroom-{}".format(i+1)
+                        channel = discord.utils.get(ctx.message.server.channels, name=channelName)
                         await bot.say("You have been allocated gameroom-" + str(i+1))
-
+                        
                         gameRooms[i].append(chosenGame)
                         gameRooms[i].append("1")
                         gameRooms[i].append(ctx.message.author.name)
-                        game = None
-                        if chosenGame == "tic-tac-toe":
-                            game = ticTacToe.TicTacToe()
-                        #other games go here
+
+                        game = GAMES[chosenGame][2]()
+
                         filename = "game" + str(i+1) + ".txt"
                         getSave.save(game, filename)
                         for player in challenged:
                             gameRooms[i].append(player)
                         getSave.save(gameRooms, "gamerooms.txt")
+                        await bot.purge_from(channel)
                         welcome = "Welcome. This is a game of " + chosenGame + " between: " + ctx.message.author.name
                         for i in range(len(challenged)):
                             if i == len(challenged) - 1:
@@ -126,70 +141,52 @@ async def newGame(ctx, *args):
                         break
                 if not success:
                     await bot.say("There are no gamerooms available, please try again later.")
-            else:
-                await bot.say("Those are not valid players!")
-        else:
-            await bot.say("I'm sorry, I don't know: \"" + args[0] + "\"")
-    else:
-        await bot.say("I'm sorry, I didn't understand that.")
 
 @bot.command(pass_context=True)
 async def place(ctx, move : int):
     """places a piece on the tic-tac-toe board"""
-    channel = ctx.message.channel.name
-    room = 0
-    if channel == "gameroom-1":
-        room = 1
-    elif channel == "gameroom-2":
-        room = 2
-    elif channel == "gameroom-3":
-        room = 3
-    elif channel == "gameroom-4":
-        room = 4
+    channelName = ctx.message.channel.name
 
-    filename = "game" + str(room) + ".txt"
-    gameRooms = getSave.get("gamerooms.txt")
-    theBoard = getSave.get(filename)
-    if room > 0:
+    syms = {0: 'X', 1: 'O'}
+
+    try:
+        room = int(channelName[-1])
+    except ValueError:
+        room = None 
+
+    if not room: 
+        await bot.say("You, sir, are not in a game room.")
+    else:
+        filename = "game{}.txt".format(room)
+        gameRooms = getSave.get("gamerooms.txt")
+        theBoard = getSave.get(filename)
         turn = int(gameRooms[room-1][1])
         if ctx.message.author.name == gameRooms[room-1][((turn + 1) % 2) + 2]:
-            sym = ""
-            if turn % 2 == 1:
-                sym = "X"
-            else:
-                sym = "O"
+            sym = syms[(turn + 1) % 2]
             try:
                 theBoard.place(move, sym)
-                iBoard = theBoard.render()
-                iBoardFileName = "iBoard" + str(room) + ".png"
-                iBoard.save(iBoardFileName)
+                iBoardFileName = "iBoard{}.png".format(room)
+                theBoard.render().save(iBoardFileName)
                 await bot.upload(iBoardFileName)
-                if theBoard.getWinner() == None and turn < 9:
+                winner = theBoard.getWinner()
+                if not winner and turn < 9:
                     turn += 1
                     gameRooms[room-1][1] = str(turn)
                     getSave.save(theBoard, filename)
                     getSave.save(gameRooms, "gamerooms.txt")
                 else:
-                    outcome = ""
-                    if theBoard.getWinner() == "X":
-                        await bot.say("X gon' give it to ya, since it won!\nVictory for: " + gameRooms[room-1][2])
-                        outcome = gameRooms[room-1][2] +  " has beaten " + gameRooms[room-1][3] + " at tic-tac-toe."
-                    elif theBoard.getWinner() == "O":
-                        await bot.say("OMG O won!!!\nVictory for: " + gameRooms[room-1][3])
-                        outcome = gameRooms[room-1][3] + " has beaten " + gameRooms[room-1][2] + " at tic-tac-toe."
+                    if winner == "X":
+                        await bot.say("X gon' give it to ya, since it won!\nVictory for: {} ".format(gameRooms[room-1][2]))
+                    elif winner == "O":
+                        await bot.say("OMG O won!!!\nVictory for: {}".format(gameRooms[room-1][3]))
                     else:
                         await bot.say("It's a tie.")
-                        outcome = gameRooms[room-1][2] + " and " + gameRooms[room-1][3] + " have tied at tic-tac-toe."
-                    lobby = discord.utils.get(bot.get_all_channels(), server__name=serverName, name='lobby')
-                    await bot.send_message(lobby, outcome)
                     gameRooms[room-1] = []
                     getSave.save(gameRooms, "gamerooms.txt")
             except ticTacToe.IllegalMoveError as er:
                 await bot.say("IllegalMoveError caught: {}".format(er.message))
         else:
             await bot.say("It ain't your turn sunshine.")
-    else:
-        await bot.say("You, sir, are not in a gameroom.")
 
 with open(TOKEN) as token:
     token = token.read()
